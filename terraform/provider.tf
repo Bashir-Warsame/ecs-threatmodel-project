55bab698a4486b3e8bb9df1edb4796a50352b53f
Bashir Warsame <121364445+Bashir-Warsame@users.noreply.github.com>
2026-02-23 16:46:08 +0000

    terraform fix

---
+++ terraform/provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  backend "s3" {
+    bucket         = "threatmodel-terraform"  
+    key            = "terraform/terraform.tfstate"
+    region         = "us-east-1"
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.3.0"
+}
+
+provider "aws" {
+  region = var.location
+}
+
